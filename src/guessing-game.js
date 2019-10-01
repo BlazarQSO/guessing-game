75e7086f561762501b1c52f5df097b887af469cd
BlazarQSO <BlazarQSO@gmail.com>
2019-10-01 05:22:35 +0300

    guessing-game/feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    letructor() {
+        this.middle = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.middle = this.min + Math.round((this.max - this.min) / 2);
+        return this.middle;
     }
 
     lower() {
-
+        this.max = this.middle;
     }
 
     greater() {
-
+        this.min = this.middle;
     }
 }
 
